b3f7f2c2f0ad04a1b667f2c23f7fbfd5530baf17
Samir El Hassan <91634008+samirelhassann@users.noreply.github.com>
2021-11-11 17:25:05 -0300

    Add project files.

---
+++ FormsGalleryApp/FormsGalleryApp.Android/SplashActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace FormsGalleryApp.Droid
+{
+    [Activity(Label = "SplashActivity", Theme = "@style/MainTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Finish();
+
+            // Disable activity slide-in animation
+            OverridePendingTransition(0, 0);
+        }
+    }
+}
